f8844c96f962258499fb78e7b7e37421f3489008
hatimgamr <135357684+hatimgamr@users.noreply.github.com>
2023-07-10 19:01:08 +0700

    Add files via upload

---
+++ tugas12.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+
+            <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+            
+                
+            </div>
+            <div class="col-sm-9">
+                <p><b> dd-mm-yy siti aisah</b></p>
+                <h3>big title </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis et animi, quis necessitatibus dicta repellendus accusamus dignissimos iste laborum ex facilis esse? Molestias doloremque error voluptas distinctio nostrum corrupti tenetur!</p>
+
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-2-p-t-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>  
+            </div>
+
+            <br>
+
+            <div class="row">
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>  
+            </div>
+            <br>
+
+            <div class="row">
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>
+                <div class="col-sm-2">
+
+                <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+
+                </div>  
+            </div>
+            
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+
+            <img src="https://placehold.co/200x200" alt="" class="img-thumbnail">
+            
+                
+            </div>
+            <div class="col-sm-9">
+                <p><b> dd-mm-yy siti aisah</b></p>
+                <h3>big title </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis et animi, quis necessitatibus dicta repellendus accusamus dignissimos iste laborum ex facilis esse? Molestias doloremque error voluptas distinctio nostrum corrupti tenetur!</p>
+
+            </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html> 
